b5d0f8b9d7bc31d806773cbf5d60117a90e211f1
Zhe Luan <joshualuan@gmail.com>
2018-11-27 04:52:05 -0500

    adding a scheloton of SGD

---
+++ SGD.py
@@ -0,0 +1,26 @@
+import numpy as np
+import math
+
+class logSGD:
+
+    def __init__(self, train_set, test_set, learn_rate):
+        self.train_set = train_set
+        self.test_set = test_set
+        self.learn_rate = learn_rate
+        self.w = []
+    
+    def computeLearnRate(rho, M):
+        learn_rate = []
+
+        return learn_rate
+    
+    def learn(self):
+        pass
+
+    def output(self):
+        w_head = []
+
+        return w_head
+    
+    def getW(self):
+        return self.w
